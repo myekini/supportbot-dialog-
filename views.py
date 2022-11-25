3eafc4dde2c16bbe52112325ee49f46d9c06a87a
Muhammad Yekini <44311524+myekini@users.noreply.github.com>
2022-11-25 06:28:13 +0100

    updates

--- views.py
+++ views.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def webhook():
     req = request.get_json(silent=True, force=True)
     res = processRequest(req)
-    print(res.sessionID)
+    print(req)
     res = json.dumps(res, indent=4)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
