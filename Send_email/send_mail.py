73011fea7308a7a72d2d3b84ab0d69e318ffe97e
Muhammad Yekini <44311524+myekini@users.noreply.github.com>
2022-11-25 08:49:12 +0100

    updates

--- Send_email/send_mail.py
+++ Send_email/send_mail.py
@@ -2,6 +2,7 @@ from email.message import EmailMessage
 import ssl
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 class EmailSender:
 
     def send_email_to_student(self, r_email, message):
@@ -9,15 +10,19 @@ class EmailSender:
         email_sender = "myekini1@gmail.com"
         email_receiver = r_email
         
+    
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "$ATL BALANCE"
+        message["From"] = "myekini1@gmail.com"
+        message["To"] = r_email
+
+        # Turn these into plain/html MIMEText objects
+        part = MIMEText(message, "html")
         
-        email = EmailMessage()
-        email['From '] = "myekini1@gmail.com"
-        email['To'] = r_email
-        email['Subject'] = "$ATL BALANCE"
-        
-        # attach the body with the msg instance
-        email_message = MIMEText(message, 'html')
-        email.set_content(message)
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(part)
+
         
         # Add SSL (layer of security)
         context = ssl.create_default_context()
@@ -26,15 +31,13 @@ class EmailSender:
         # Log in and send the email
         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
             smtp.login(email_sender, email_password)
-            smtp.sendmail(email_sender, email_receiver, email.as_string())
+            smtp.sendmail(email_sender, email_receiver, message.as_string())
     
     def send_email_to_support(self):
         return "support working"
 
             
 
-
-        
 
 
             
