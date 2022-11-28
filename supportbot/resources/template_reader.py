9405d9624eb2bb40258007343d517b81485fdf5d
Muhammad Yekini <44311524+myekini@users.noreply.github.com>
2022-11-28 21:21:07 +0100

    updates

--- supportbot/resources/template_reader.py
+++ supportbot/resources/template_reader.py
@@ -5,10 +5,10 @@ class TemplateReader:
     def read_course_template(self, nft_type):
         try:
             if (nft_type=='acre'):
-                email_file = open(r"supportbot\resources\email_template\acre_balance.html", "r")
+                email_file = open(r"supportbot/resources/email_template/acre_balance.html", "r")
                 email_message = email_file.read()
             elif (nft_type=='plot'):
-                email_file = open(r"supportbot\resources\email_template\plot_balance.html", "r")
+                email_file = open(r"supportbot/resources/email_template/plot_balance.html", "r")
                 email_message = email_file.read()
             elif (nft_type == 'yard'):
                 email_file = open(r"supportbot\resources\email_template\yard_balance.html", "r")
