df9266459e57c63bd6466036a4b0b3b7534187d0
Muhammad Yekini <44311524+myekini@users.noreply.github.com>
2023-02-06 21:16:12 +0100

    updates

---
+++ supportbot/resources/apis/youtube.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+from decouple import config
+from googleapiclient.discovery import build
+
+
+
+def youtube():
+    api_key = config("api_key")
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    request = youtube.search().list(
+        part= "snippet",
+        channelId = config("channel_id"),
+        q = "COA",
+        type = "video",
+        order = "title"
+        )
+
+    response = request.execute()
+    full_res = response['items']
+    for f in full_res:
+        print(f['snippet']['title'])
